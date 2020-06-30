ce74efc486300ceb62b14869508f7555bbb6608c
Amanda Coradini <amandaa.coradini@gmail.com>
2020-06-30 00:05:05 -0300

    Alterando pastas

--- src/teste_import.py
+++
@@ -1,4 +0,0 @@
-from funcoes_met_cla import Calculadora2
-
-calc = Calculadora2()
-print(calc.multiplica1(10, 5))
--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\amand\documents\curso-html\projetopython\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
